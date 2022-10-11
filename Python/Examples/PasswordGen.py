3b1faf5946de189245fb4f36dc3c35c95af96f4a
ShalomVanunu <shalomvanunu@gmail.com>
2022-10-11 12:01:25 +0300

    Updated 3/10

--- Python/Examples/PasswordGen.py
+++ Python/Examples/PasswordGen.py
@@ -7,16 +7,23 @@
 ## לפחות גדול מ- 4
 import string
 import random
-upper = "ABCDEFGH"
-print(random.choice(upper))
+
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+punctuation = string.punctuation
+password = ""
 
 while True:
-    password_len =  int(input("write the length of Password\n"))
+    password_len =  int(input("write the length of Password \n"))
 
-    if password_len>4 :
-        pass
+    if password_len>=6 :
+        password +=random.choice(upper)
+        for _ in range(password_len-2):
+            password += random.choice(lower)
+        password += random.choice(string.punctuation)
+        print(password)
     else:
-        print("enter password length grater than 4")
+        print("enter password length grater than 6")
         continue
 
 
