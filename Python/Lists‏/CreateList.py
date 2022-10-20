238627914d05d9f2b44225f706cca75261ad3bc9
ShalomVanunu <shalomvanunu@gmail.com>
2022-10-20 12:23:12 +0300

    Updated 20/10

--- Python/Lists‏/CreateList.py
+++ Python/Lists‏/CreateList.py
@@ -1,11 +1,11 @@
 
+
 ages = []
 
 for _ in range(5):
     age = input("enter your age :")
     ages.append(age)
 
-print(ages)
 ages[0] ="100"
 ages.insert(1,'22') # add value and shift the list right
 print(ages)
