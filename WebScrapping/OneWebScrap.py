c7de9368244e2c47912050382806d8858199f98a
shalomvanunu <shalomvanunu@gmail.com>
2022-12-03 19:17:25 +0200

    Update 3/12/22

--- WebScrapping/OneWebScrap.py
+++ WebScrapping/OneWebScrap.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL  = "https://www.sport5.co.il/" # URL that i want to get the data a
+URL  = "https://worldcup.sport5.co.il/TemplatesExternal/Mondial/MondialStage.aspx?folderid=10795" # URL that i want to get the data
 
 html_site = requests.get(URL).content # HTML DATA from site in Bytes
 
@@ -10,6 +10,6 @@ soup = BeautifulSoup(html_site, "html.parser") # transfer Bytes to HTML stractur
 
 
 #
-result = soup.find_all(class_="img-flag-container")
+result = soup.find_all("h3")
 for i in result:
-    print(i.text)
+    print(i.get_text())
