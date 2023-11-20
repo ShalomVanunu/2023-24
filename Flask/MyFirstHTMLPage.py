5b0cfeb6fee97ab9b6e5c85f8537682ef7412042
sshalomvanunu <shalomvanunu@gmail.com>
2023-11-20 15:47:33 +0200

    Upload 20/11

---
+++ Flask/MyFirstHTMLPage.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+name = "moshe"
+list_names = ["moshe", "shalom", "Itzik"]
+@app.route("/")
+def first_page():
+    return render_template("index.html", name=name,listnames = list_names)
+
+@app.route("/bank")
+def bank_hapoalim():
+    return render_template("בנק הפועלים - כניסה לחשבונך.html")
+
+if __name__ == "__main__":
+    app.run(port =80,debug=True,host="192.168.1.120")
---
+++ Flask/MyFirstWebApp.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return "<h1> Hi, Wellcome My First Page </h1>"
+
+@app.route("/name")
+def my_name():
+    return "<h2> My name is Shalom </h2>"
+
+if __name__ == "__main__":
+    app.run(port =80,debug=True,host="192.168.1.120")
